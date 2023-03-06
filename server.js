f047de262c927c865d0bf51f8886e52693084a48
Patrick McKnight <patrickgmcknight@gmail.com>
2023-03-05 19:29:36 -0800

    completed get requests for notes.html and db.json files

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const fs = require ('fs');
+const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db', 'db.json'));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'notes.html'));
+})
+
+app.post('/api/notes', (req, res) => {
+    let addNote = req.body;
+    let listNotes = JSON.parse(fs.readFileSync('./db/db.json', "utf8"));
+    let notesLength = (listNotes.length).toString();
+    
+})
