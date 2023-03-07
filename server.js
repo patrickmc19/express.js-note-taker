8b46e9d9263b8833195cec67883444f4cf00b54d
Patrick McKnight <patrickgmcknight@gmail.com>
2023-03-06 21:25:11 -0800

    updated app.post to db.json function syntax

--- server.js
+++ server.js
@@ -1,30 +1,56 @@
 const express = require('express');
-const fs = require ('fs');
+const fs = require('fs');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db', 'db.json'));
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+    console.info(`${req.method} request received to get notes`);
 })
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public', 'notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
 
 app.post('/api/notes', (req, res) => {
-    let addNote = req.body;
-    let listNotes = JSON.parse(fs.readFileSync('./db/db.json', "utf8"));
-    let notesLength = (listNotes.length).toString();
-    addNote.id = notesLength;
-    listNotes.push(addNote);
-    fs.writeFileSync("./db/db.json", JSON.stringify(listNotes));
-    res.json(listNotes);
-})
+    console.info(`${req.method} received to add a new note`);
+    const { title, text } = req.body;
+    if (title && text) {
+        const addNote = {
+            title,
+            text,
+            // review_id: uuid(), - how do you include this?
+        }
+        fs.readFile('/db/db.json', "utf8", (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+                parsedNotes.push(addNote);
 
-app.listen(PORT);
+                fs.writeFile('/db/db.json', JSON.stringify(parsedNotes, null, 3), (err) =>
+                err ? console.error(err):
+                cnonsole.log(`Note for ${addNote.title} has been written to JSON file`)
+                )
+            }
+        });
+        const response = {
+            status: "success",
+            body: addNote,
+        };
+        console.log(response);
+        res.status(201).json(response);    
+    } else {
+        res.status(500).json("Error in creating new note");
+    }
+});
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
